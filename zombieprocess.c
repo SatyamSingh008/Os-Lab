afed7969a58e2c4f3ab15d8da2455e8f95de1aa8
SatyamSingh008 <161092666+SatyamSingh008@users.noreply.github.com>
2024-10-23 10:58:39 +0530

    Add files via upload

---
+++ zombieprocess.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int main(){
+	pid_t pid = fork();
+	if(pid > 0) {
+		sleep(50);
+	}
+	else {
+		
+		exit(0);
+	}
+	return 0;
+}
